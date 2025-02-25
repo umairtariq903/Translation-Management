24d98efe59f5b2e9e421295f09a8e67e94fd5e89
umairtariq903 <umairtariq903@yahoo.com>
2025-02-25 06:36:06 -0800

    Updated optimization

--- app/Http/Controllers/TranslationController.php
+++ app/Http/Controllers/TranslationController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Translation;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Cache;
 class TranslationController extends Controller
 {
     /**
